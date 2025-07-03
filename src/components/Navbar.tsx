d77593ef9dd77ae0d5b5aef66352eacc25363715
Kasiajay553 <143077804+Kasiajay553@users.noreply.github.com>
2025-07-03 13:58:11 +0530

    Update  resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -97,7 +97,7 @@ const Navbar: React.FC = () => {
               </a>
             ))}
             <a
-              href="/resume.pdf"
+              href="/KASI_AJAY_RESUME.pdf"
               download
               className="inline-flex items-center space-x-2 mt-4 bg-blue-600 text-white px-4 py-2 rounded-full text-sm font-medium hover:bg-blue-700 transition-colors"
             >
