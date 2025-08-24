733944c7c468bc5ec523b4fc2ebaceeb640cb0c5
Kasiajay553 <143077804+Kasiajay553@users.noreply.github.com>
2025-08-24 18:32:07 +0530

    Update RESUME

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -60,7 +60,7 @@ const AboutSection: React.FC = () => {
 
             <div className="pt-6">
               <motion.a
-                href="/resume.pdf"
+                href="/AJAY_RESUME.pdf"
                 download="AJAY_RESUME.pdf"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
